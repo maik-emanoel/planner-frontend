afa1179e98bb8a924e51d2439ebba02f7e0ae259
Maik Emanoel <maikemanoel11@gmail.com>
2024-08-04 12:00:55 -0300

    feat: add create-trip page

---
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -0,0 +1,45 @@
+import { UserRoundPlus, ArrowRight } from 'lucide-react'
+import { Button } from '../../../components/button'
+
+interface InviteGuestsStepProps {
+  openGuestsModal: () => void
+  emailsToInvite: string[]
+  openConfirmTripModal: () => void
+}
+
+export function InviteGuestsStep({
+  emailsToInvite,
+  openConfirmTripModal,
+  openGuestsModal,
+}: InviteGuestsStepProps) {
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+      <button
+        onClick={openGuestsModal}
+        type="button"
+        className="flex items-center gap-2 flex-1"
+      >
+        <UserRoundPlus className="size-5 text-zinc-400" />
+        {emailsToInvite.length ? (
+          <span className="text-lg text-zinc-100 flex-1 text-left">
+            {emailsToInvite.length} pessoa(s) convidada(s)
+          </span>
+        ) : (
+          <span className="text-lg text-zinc-400 flex-1 text-left">
+            Quem estará na viagem?
+          </span>
+        )}
+      </button>
+
+      <div className="w-px h-6 bg-zinc-800" />
+
+      <Button
+        onClick={openConfirmTripModal}
+        variant='primary'
+      >
+        Confirmar viagem
+        <ArrowRight className="size-5" />
+      </Button>
+    </div>
+  )
+}
