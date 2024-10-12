995f60d55b7dacf95368eb1b1df7d475282fd10f
Maik Emanoel <maikemanoel11@gmail.com>
2024-10-12 14:48:52 -0300

    feat: get activities

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,9 +1,60 @@
-import { CircleCheck } from "lucide-react";
+import { CircleCheck } from 'lucide-react'
+import { api } from '../../libs/axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Activity } from '../../types'
+import { dayjs } from '../../libs/dayjs'
 
 export function Activities() {
+  const { tripId } = useParams()
+  const [activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities))
+  }, [tripId])
+
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
+      {activities.map((category, index) => {
+        return (
+          <div key={index} className="space-y-2.5">
+            <div className="flex items-baseline gap-2">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia {dayjs(category.date).format('DD')}
+              </span>
+              <span className="text-xs text-zinc-500 first-letter:uppercase">
+                {dayjs(category.date).format('dddd')}
+              </span>
+            </div>
+
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {dayjs(activity.occurs_at).format('HH:mm')}h
+                        </span>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
+            )}
+          </div>
+        )
+      })}
+
+      {/* <div className="space-y-2.5">
         <div className="flex items-baseline gap-2">
           <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
           <span className="text-xs text-zinc-500">Sábado</span>
@@ -35,7 +86,7 @@ export function Activities() {
             <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
